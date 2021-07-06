98a0bc3f871ff87e51fbfc83fa84f6057bda8c25
nsxsnx <51633941+nsxsnx@users.noreply.github.com>
2021-07-06 08:29:13 +0300

    Add files via upload

---
+++ logoff.ps1
@@ -0,0 +1,23 @@
+CLS
+$RD = Get-RDUserSession -ConnectionBroker Ariadna.mures.global -CollectionName "MURES"
+foreach ($item in $RD) 
+{
+    $UsessionID = $item.UnifiedSessionId -as [int]
+    $sessionID = $item.SessionId -as [int]
+    if ($item.SessionState -eq "STATE_DISCONNECTED" -and $item.DisconnectTime -ne $null -and $item.UnifiedSessionId -ne $null)
+    {
+        #if ($item.UserName -ne "testuser")  {continue}
+        write-host $Item.UserName " " -ForegroundColor "magenta" -NoNewline
+        write-host  $item.SessionState "since" $item.DisconnectTime -NoNewline
+        write-host " Unified SId:" $UsessionID "User SId:" $sessionID -ForegroundColor "darkcyan" -NoNewline
+        write-host " Host" $item.ServerName  "on" $item.HostServer -ForegroundColor "cyan"
+        $TimeDiff = New-TimeSpan -start $item.DisconnectTime -end (Get-Date)
+        if ($TimeDiff.Minutes -lt 5) 
+        {
+            write-host "Disconnected for less than 5 minutes" -ForegroundColor "Green"
+            continue
+        }
+        #Invoke-RDUserLogoff -HostServer $item.HostServer -UnifiedSessionID $UsessionID -Force -verbose #-erroraction 'silentlycontinue'
+        write-host "killed" -ForegroundColor "red"
+        }
+}
